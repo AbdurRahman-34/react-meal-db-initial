598139dd149f24dbcff7b2e362456b686e23604e
AbdurRahman-34 <get.abdurahman@gmail.com>
2022-09-30 22:48:02 +0600

    Header heading style update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h2>Welcome to My Restaurant</h2>
+            <h1 style={{color : '#1E1747'}}>Welcome to My Restaurant</h1>
             <h4>Please order the food you like</h4>
         </div>
     );
